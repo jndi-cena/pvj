41adfcee7ac9ce59dbac00bc913ac2d9fa401d80
jndi cena <spam@someoneelse.com>
2022-08-10 09:59:48 -0700

    update smbcheck to reg key

--- purger.ps1
+++ purger.ps1
@@ -10,17 +10,20 @@ function SMBCheck() {
    if ($SMBv1Enabled.State -eq "Enabled") {
       Write-Host "[ + ] SMBv1 Enabled."
       Write-Host "[ + ] Disabling SMBv1..."
-      Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+      # Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol
+      Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1 -Type DWORD -Value 0 -Force
    }
 
    # Check to make sure SMBv1 was properly disabled, and not automatically restarted by a rogue service/process
-   $SMBv1Enabled = Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol | Select-Object State
+   # $SMBv1Enabled = Get-WindowsOptionalFeature -Online -FeatureName SMB1Protocol | Select-Object State
+   $SMBv1Enabled = Get-Item HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters | ForEach-Object {Get-ItemProperty $_.pspath} | Select SMB1.SMB1
 
    if ($SMBv1Enabled.State -eq "Enabled") {
       Write-Host "[ - ] Unable to disable SMBv1. Check host for rogue services or processes that may be restarting it automatically."
    }
    else {
-      Write-Host "[ + ] SMBv1 is disabled."
+      Write-Host "[ + ] SMBv1 is disabled. Rebooting machine..."
+      shutdown /r /t 0 
    }
 }
 
@@ -39,6 +42,15 @@ function SMBCheck() {
    foreach($User in $MatchedADUsers.Name){
       Write-Host "Deleting $User"
       Remove-ADUser -Confirm:$false -Identity $User
+   }
+ }
+
+ function PurgeService() {
+   $MatchedService = Get-Service | Where-Obect {$_.Name -like "*$Service*"}
+   
+   foreach($Service in $MatchedService.Name) {
+      Stop-Service -Force -Confirm -NoWait -Name $Service
+      
    }
  }
 
